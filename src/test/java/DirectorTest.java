7a0b0b0ab8bd264b5db9c083f7244165ff9a8d23
CrugBarat <craigtbarratt@gmail.com>
2020-06-02 02:01:19 +0100

    tests Director class - PASSED
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/test/java/DirectorTest.java
@@ -0,0 +1,51 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.management.Director;
+
+import static org.junit.Assert.assertEquals;
+
+public class DirectorTest {
+
+    Director director;
+
+    @Before
+    public void setUp() {
+        director = new Director("Neil Godwin", "FT921756P", 3500.00, "Management", 200000.00);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Neil Godwin", director.getName());
+    }
+
+    @Test
+    public void hasNINumber() {
+        assertEquals("FT921756P", director.getNationalInsuranceNumber());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(3500.00, director.getSalary(), 0.1);
+    }
+
+    @Test
+    public void hasDepartment() {
+        assertEquals("Management", director.getDeptName());
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        director.raiseSalary(350.00);
+        assertEquals(3850.00, director.getSalary(), 0.1);
+    }
+
+    @Test
+    public void hasPayBonus() {
+        assertEquals(70.00, director.payBonus(), 0.1);
+    }
+
+    @Test
+    public void hasBudget() {
+        assertEquals(200000.00, director.getBudget(), 0.1);
+    }
+}
