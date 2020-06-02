7eed850e4609c436895f86e5e3965545b8d9c2de
CrugBarat <craigtbarratt@gmail.com>
2020-06-02 01:44:21 +0100

    tests DatabaseAdmin class - PASSED
    Signed-off-by: CrugBarat <craigtbarratt@gmail.com>

---
+++ src/test/java/DatabaseAdminTest.java
@@ -0,0 +1,41 @@
+import org.junit.Before;
+import org.junit.Test;
+import staff.techStaff.DatabaseAdmin;
+
+import static org.junit.Assert.assertEquals;
+
+public class DatabaseAdminTest {
+
+    DatabaseAdmin databaseAdmin;
+
+    @Before
+    public void setUp() {
+        databaseAdmin = new DatabaseAdmin("Dawn Tinsley", "DW129856X", 1200.00);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Dawn Tinsley", databaseAdmin.getName());
+    }
+
+    @Test
+    public void hasNINumber() {
+        assertEquals("DW129856X", databaseAdmin.getNationalInsuranceNumber());
+    }
+
+    @Test
+    public void hasSalary() {
+        assertEquals(1200.00, databaseAdmin.getSalary(), 0.1);
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        databaseAdmin.raiseSalary(120.00);
+        assertEquals(1320.00, databaseAdmin.getSalary(), 0.1);
+    }
+
+    @Test
+    public void hasPayBonus() {
+        assertEquals(12.00, databaseAdmin.payBonus(), 0.1);
+    }
+}
